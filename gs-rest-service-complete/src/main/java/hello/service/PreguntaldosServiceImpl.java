58dd6d24707fc670ede88ccdf547469c83a208df
Java <arielsilvainfo@gmail.com>
2019-05-20 21:10:38 -0300

    Sync clase 14

--- gs-rest-service-complete/src/main/java/hello/service/PreguntaldosServiceImpl.java
+++ gs-rest-service-complete/src/main/java/hello/service/PreguntaldosServiceImpl.java
@@ -1,29 +1,41 @@
 package hello.service;
 
+import org.springframework.stereotype.Service;
+
 import hello.dao.PreguntaldosDAO;
 import hello.dao.PreguntaldosMemoryDAO;
 import hello.model.PreguntaRespuesta;
 
+@Service
 public class PreguntaldosServiceImpl implements PreguntaldosService {
 
 	@Override
 	public String getPregunta(Integer id) {
 		PreguntaldosDAO dao = new PreguntaldosMemoryDAO();
 		PreguntaRespuesta pr = dao.getPreguntaRespuesta(id);
-		
+		pr = new PreguntaRespuesta();
+		pr.setId(id);
+		pr.setPregunta("Como se llama?");
+		pr.setRespuesta("Ud sabe");
 		return pr.getPregunta();
 	}
 
 	@Override
 	public String getRespuesta(Integer id) {
-		// TODO Auto-generated method stub
-		return null;
+		PreguntaRespuesta pr = new PreguntaRespuesta();
+		pr.setId(id);
+		pr.setPregunta("Como se llama?");
+		pr.setRespuesta("Ud sabe");
+		return pr.getRespuesta();
 	}
 
 	@Override
 	public PreguntaRespuesta getPreguntaRespuesta(Integer id) {
-		// TODO Auto-generated method stub
-		return null;
+		PreguntaRespuesta pr = new PreguntaRespuesta();
+		pr.setId(id);
+		pr.setPregunta("Como se llama?");
+		pr.setRespuesta("Ud sabe");
+		return pr;
 	}
 
 	@Override
