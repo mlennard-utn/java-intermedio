aa94453d608c43f277f2eaa58921a94d0b4ca8ed
Java <arielsilvainfo@gmail.com>
2019-05-20 21:49:53 -0300

    Clase 14

--- gs-rest-service-complete/src/main/java/hello/service/PreguntaldosServiceImpl.java
+++ gs-rest-service-complete/src/main/java/hello/service/PreguntaldosServiceImpl.java
@@ -1,40 +1,35 @@
 package hello.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import hello.dao.PreguntaldosDAO;
 import hello.dao.PreguntaldosMemoryDAO;
 import hello.model.PreguntaRespuesta;
 
 @Service
 public class PreguntaldosServiceImpl implements PreguntaldosService {
 
+	@Autowired
+	PreguntaldosMemoryDAO dao;
+	
 	@Override
 	public String getPregunta(Integer id) {
-		PreguntaldosDAO dao = new PreguntaldosMemoryDAO();
 		PreguntaRespuesta pr = dao.getPreguntaRespuesta(id);
-		pr = new PreguntaRespuesta();
-		pr.setId(id);
-		pr.setPregunta("Como se llama?");
-		pr.setRespuesta("Ud sabe");
+		
 		return pr.getPregunta();
 	}
 
 	@Override
 	public String getRespuesta(Integer id) {
-		PreguntaRespuesta pr = new PreguntaRespuesta();
-		pr.setId(id);
-		pr.setPregunta("Como se llama?");
-		pr.setRespuesta("Ud sabe");
+		PreguntaRespuesta pr = dao.getPreguntaRespuesta(id);
+		
 		return pr.getRespuesta();
 	}
 
 	@Override
 	public PreguntaRespuesta getPreguntaRespuesta(Integer id) {
-		PreguntaRespuesta pr = new PreguntaRespuesta();
-		pr.setId(id);
-		pr.setPregunta("Como se llama?");
-		pr.setRespuesta("Ud sabe");
+		PreguntaRespuesta pr = dao.getPreguntaRespuesta(id);
+		
 		return pr;
 	}
 
