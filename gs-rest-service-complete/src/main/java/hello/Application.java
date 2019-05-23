197e45e5a2997c00ea43116c5dcfaccc7cd42b25
Martin Lennard <martin.lennard@mercadolibre.com>
2019-05-23 14:10:42 -0300

    Incluye proceso que arma las preguntas en memoria

--- gs-rest-service-complete/src/main/java/hello/Application.java
+++ gs-rest-service-complete/src/main/java/hello/Application.java
@@ -1,8 +1,17 @@
 package hello;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.core.io.ClassPathResource;
+
+import hello.dao.PreguntaldosMemoryDAO;
+import hello.model.PreguntaRespuesta;
 
 @SpringBootApplication
 @ComponentScan({"hello","hello.dao","hello.service","hello.model"})
@@ -11,4 +20,25 @@ public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
+    
+    
+    @Bean
+    CommandLineRunner initDatabase(PreguntaldosMemoryDAO repository) {
+        return args -> {
+        	PreguntaRespuesta pr = null;
+        	BufferedReader reader =
+                    new BufferedReader(new FileReader( new ClassPathResource("Preguntas_Guido.tsv").getFile()));
+                String ln;
+                while((ln = reader.readLine()) != null) {
+                    String[] record = ln.split("	");
+                    pr = new PreguntaRespuesta();
+                    pr.setPregunta(record[0]);
+                    pr.setRespuesta(record[1]);
+                    repository.insertPreguntaRespuesta(pr);
+                }
+        	
+        	
+        };
+    }
+    
 }
--- gs-rest-service-complete/src/main/java/hello/dao/PreguntaldosMemoryDAO.java
+++ gs-rest-service-complete/src/main/java/hello/dao/PreguntaldosMemoryDAO.java
@@ -1,5 +1,6 @@
 package hello.dao;
 
+import java.util.HashMap;
 import java.util.concurrent.atomic.AtomicLong;
 
 import org.springframework.stereotype.Repository;
@@ -9,14 +10,24 @@ import hello.model.PreguntaRespuesta;
 @Repository
 public class PreguntaldosMemoryDAO implements PreguntaldosDAO {
 
+	private HashMap<Integer, PreguntaRespuesta> preguntas = new HashMap<>();
+	private AtomicLong counter = new AtomicLong(0);
+	
 	@Override
 	public Integer insertPreguntaRespuesta(PreguntaRespuesta pr) {
-		AtomicLong counter = new AtomicLong();
-		return (int)counter.getAndIncrement();
+		Integer id = (int)counter.getAndIncrement();
+		pr.setId(id);
+		preguntas.put(id, pr);
+		return id;
+				
 	}
 
 	@Override
 	public Boolean deletePreguntaRespuesta(Integer id) {
+		if (preguntas.containsKey(id)) {
+			preguntas.remove(id); 
+			return Boolean.TRUE;
+		}
 		return Boolean.FALSE;
 	}
 
@@ -27,11 +38,9 @@ public class PreguntaldosMemoryDAO implements PreguntaldosDAO {
 
 	@Override
 	public PreguntaRespuesta getPreguntaRespuesta(Integer id) {
-		PreguntaRespuesta pr = new PreguntaRespuesta();
-		pr.setId(id);
-		pr.setPregunta("Como se llama?");
-		pr.setRespuesta("Ud sabe");
-		return pr;
+		
+		return preguntas.get(id);
 	}
+	
 
 }
